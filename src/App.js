b16ba241591c7ef9ce78428981cc855dbf55a176
aliahmadi5578 <aliahmadi5578@yahoo.com>
2025-05-14 18:42:52 +0330

    fix bugs

--- src/App.js
+++ src/App.js
@@ -1,41 +1,16 @@
-import {
-  BrowserRouter,
-  Route,
-  Router,
-  Routes,
-  Navigate,
-} from "react-router-dom";
+import { Route, Routes, HashRouter } from "react-router-dom";
 import "./App.css";
-import Login from "./login/Login";
-import Home from "./home/Home";
-import { HeProtectedRoute } from "./utility/HeProtectedRoute";
 import { ToastContainer } from "react-toastify";
-import CafeLog from "./log/cafeLog";
-import Kisok from "./kiosk/Kiosk";
-import ProductsTable from "./products/ProductsTable";
-import Inventory from "./products/inventory";
 import AttendanceApp from "./HR/AttendanceApp";
 
 export default function App() {
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
-          <Route path="/kiosk" exact element={<Kisok />} />
-          <Route path="/products" exact element={<ProductsTable />} />
-          <Route path="/inventory" exact element={<Inventory />} />
-          <Route path="/" exact element={<AttendanceApp />} />
-          {/* <Route
-            path="*"
-            exact
-            element={
-              <HeProtectedRoute>
-                <Navigate to="/" />
-              </HeProtectedRoute>
-            }
-          /> */}
+          <Route path="" exact element={<AttendanceApp />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
       <ToastContainer
         position="bottom-left"
         autoClose={1000}
